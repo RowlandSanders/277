d8054be16814ec2794a7ad9d72961a870914fcc4
Rowland Sanders <48600494+RowlandSanders@users.noreply.github.com>
2020-09-16 19:06:18 -0700

    Update Main.java

--- 277 Solitaire Prime/Main.java
+++ 277 Solitaire Prime/Main.java
@@ -1,8 +1,12 @@
 /*
+
 Rowland Sanders
-CECS 277
+Prog - CECS 277-01
 Solitaire Prime
 09/08/2020
+ I certify that this program is my own original work. I did not copy any part of this program from
+any other source. I further certify that I typed each and every line of code in this program
+
  */
 //package CardGames;
 
